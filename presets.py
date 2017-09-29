bc32ce185a853c0788cb66e5b7ce2875bede89d1
LokiLuciferase <luxxus1@gmail.com>
2017-09-30 00:17:10 +0200

    cleanup
    Signed-off-by: LokiLuciferase <luxxus1@gmail.com>

--- presets.py
+++ presets.py
@@ -2,7 +2,7 @@
 # Created by JLT on 29/09/2017.
 #
 
-# whoever can be assed to look up factory presets, enter them here
+# whoever can be assed to look up factory presets, enter them here together with a good name
 # order of list: initial melt, final extension, melt, anneal, extension, cycles
 
 presets = {"NEB Phusion": [30, 420, 7, 15, 23, 30],
